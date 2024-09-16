1535740504c948f5e0926afe35ec96d1fc8764df
salazarsebas <sebastian.salazar.solano@gmail.com>
2024-09-15 18:21:27 -0600

    Add project files.

---
+++ PracticaWebAPI/Models/Parametros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaWebAPI.Models
+{
+    public class Parametros
+    {
+        public string continente { get; set; }
+        public string pais { get; set; }
+        public string capitalp { get; set; }
+        public int poblacion { get; set; }
+        public string bandera { get; set; }
+    }
+}
---
+++ PracticaWebAPI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PracticaWebAPI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
